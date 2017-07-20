4c70b915b8bff70e4c3a8fa93a78c04075f69fdc
Caio Ladislau <caiolaszlo@gmail.com>
2017-07-20 13:50:54 -0300

    Adiciona send do Lealjs no CactoJS

--- js/cacto.js
+++ js/cacto.js
@@ -153,3 +153,109 @@ var localDB = {
   }
 
 };
+
+/**
+ * Necessário declarar o objeto para que não ocorram erros
+ */
+$.support.cors = true;
+var serverUrl = "/";
+
+$.each(["put", "delete", "post", "get"], function(i, method) {
+  $[method] = function(url, data, callback, fail) {
+    var url_complete = serverUrl + url;
+    while (url_complete.indexOf("//") >= 0)
+      url_complete = url_complete.replace(/\/\//gi, "/");
+
+    return $.ajax({
+      url: url_complete,
+      type: method,
+      dataType: "json",
+      crossDomain: true,
+      data: data,
+      success: callback,
+      error: fail
+    });
+  };
+});
+
+$.fn.send = function(success, error) {
+  $(this).submit(function(e) {
+    e.preventDefault();
+    if (!$(this).validation())
+      return false;
+    // $.dialog.waiting("Aguarde...");
+    $.post($(this).attr("action"), $(this).serialize(), success, error).done(function() {
+      // if($(".dialog-waiting").length) $.dialog.close();
+    });
+    return false;
+  });
+};
+
+$.fn.sendImage = function(success, error) {
+  $(this).submit(function() {
+    $.dialog.waiting("Enviando...");
+    $.ajax({
+      type: "POST",
+      url: serverUrl + $(this).attr("action"),
+      data: new FormData(this),
+      enctype: 'multipart/form-data',
+      processData: false, // tell jQuery not to process the data
+      contentType: false // tell jQuery not to set contentType
+    }).success(success).fail(error).done(function() {
+      if ($(".dialog-waiting").length) $.dialog.close();
+    });
+    return false;
+  });
+  $(this).find('input[type="file"]').change(function() {
+    var str = $(this).val(),
+      arr = [/.png$/, /.jpg$/, /.jpge$/];
+    for (var i = 0; i < arr.length; i++)
+      if (str.search(arr[i]) > 0) {
+        $(this).closest('form').submit();
+        return;
+      }
+    $.alert("Arquivo inválido!");
+  });
+};
+
+$.fn.serializeObject = function() {
+  var obj = {};
+  var arr = this.serializeArray();
+  $.each(arr, function() {
+    if (obj[this.name]) {
+      if (!obj[this.name].push) {
+        obj[this.name] = [obj[this.name]];
+      }
+      obj[this.name].push(this.value || '');
+    } else {
+      obj[this.name] = this.value || '';
+    }
+  });
+  return obj;
+};
+
+$.fn.validation = function() {
+  var inputs = $(this).find("input, select");
+  $(inputs).removeClass('invalid');
+  $(".callback").hide();
+  for (var i = 0; i < $(inputs).length; i++) {
+    var regex = $(inputs).eq(i).attr('regex');
+    if (regex && regex.length && $(inputs).eq(i).is(":enabled")) {
+      var val = $(inputs).eq(i).is(":radio") || $(inputs).eq(i).is(":checkbox") ?
+        $(this).find("input:" + $(inputs).eq(i).attr("type")).filter("[name='" + $(inputs).eq(i).attr("name") + "']").filter(":checked").val() :
+        $(inputs).eq(i).val();
+      val = val ? val : "";
+      // console.log($(inputs).eq(i).attr("name"), val, regex);
+      if (!(new RegExp(regex, "i")).exec(val)) {
+        $(inputs).eq(i).closest(".form-group").find(".callback").show();
+        $(inputs).eq(i).addClass('invalid').focus();
+        return false;
+      } else {}
+    }
+  }
+  return true;
+};
+
+function clone(x) {
+  return JSON.parse(JSON.stringify(x));
+}
